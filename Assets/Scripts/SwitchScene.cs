3b23ecbe5e32711e3337f67b2afee4e83bb073eb
Lizzy Hwang <hwang.eli@northeastern.edu>
2024-03-30 00:11:51 -0400

    homescreen & scene switcher script

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchScene : MonoBehaviour
+{
+    private Button startButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton = GameObject.Find("StartButton").GetComponent<Button>();
+        startButton.onClick.AddListener(StartGame);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+
+}
